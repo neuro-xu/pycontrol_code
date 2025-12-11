d013adb63cb960082d38dc9ec0de7466821d13a0
Brian Xu <brian.zx.xu@gmail.com>
2025-12-11 13:06:31 -0500

    update hardware definition

--- hardware_definitions/hardware_definition.py
+++ hardware_definitions/hardware_definition.py
@@ -7,8 +7,11 @@ board = Breakout_1_2()
 
 # Instantiate Devices.
 left_poke = Poke(board.port_1, rising_event="left_poke", falling_event="left_poke_out")
+left_port = left_poke
 center_poke = Poke(board.port_2, rising_event="center_poke", falling_event="center_poke_out")
+center_port = center_poke
 right_poke = Poke(board.port_3, rising_event="right_poke", falling_event="right_poke_out")
+right_port = right_poke
 
 teensy_sync = Frame_logger(pin=board.port_4.DIO_C, rising_event="teensy_sync")
 
--- tasks/BX/setup/open_solenoids.py
+++ tasks/BX/setup/open_solenoids.py
@@ -5,9 +5,9 @@ from devices import *
 
 # Instantiate Devices.
 board = Breakout_1_2()
-left_poke = Poke(board.port_3, rising_event="left_poke", falling_event="left_poke_out")
-center_poke = Poke(board.port_4, rising_event="center_poke", falling_event="center_poke_out")
-right_poke = Poke(board.port_2, rising_event="right_poke", falling_event="right_poke_out")
+left_poke = Poke(board.port_1, rising_event="left_poke", falling_event="left_poke_out")
+center_poke = Poke(board.port_2, rising_event="center_poke", falling_event="center_poke_out")
+right_poke = Poke(board.port_3, rising_event="right_poke", falling_event="right_poke_out")
 
 # States and events.
 
